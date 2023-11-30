524a51ee1e077f5cd103fbb5daa51ff695680f02
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-11-30 23:37:03 +0900

    add import library

--- sendFiregase.py
+++ sendFiregase.py
@@ -1,10 +1,18 @@
+import firebase_admin
+from firebase_admin import firestore
+
+# firebaseに取得した情報を送信するクラス
 class SendFirebase():
     def __init__(self) -> None:
         pass
 
+    # 実際に送信するためのクラス定義
     def main(self):
+        app = firebase_admin.initialize_app()
+        db = firestore.client()
+        data = {"companyName": "sendFirebaseTest", "date": "sendFirebaseTest", "title": "sendFirebaseTest", "webLink": "sendFirebaseTest" }
+        db.collection("event").add(data)
         print("test")
-
 
 if __name__ == "__main__":
     sendFirebase = SendFirebase()
