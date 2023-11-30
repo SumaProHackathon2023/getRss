f270a52e086608fb3595eeb59784aa285d2878fd
YSassan <c1117817@planet.kanazawa-it.ac.jp>
2023-12-01 00:00:05 +0900

    titleとlinkをテキストファイルに出力

--- getRss.py
+++ getRss.py
@@ -2,6 +2,14 @@
 # 上のリンクのやつをうまいこと取得できるようにしてほしい
 # わかりづらかったら、コメントアウトめちゃめちゃ使っていいよ
 import feedparser
+import pprint
 
 url = "https://connpass.com/explore/ja.atom"
-f = feedparser.parser(url)
+f = feedparser.parse(url)
+#print(len(f))
+#pprint.pprint(f)
+file = open('myfile.txt', 'w', encoding = 'UTF-8')
+for article in f.entries:
+    file.write('title:' + article.title + ' ' + 'link:' + article.link)
+    file.write('\n')
+file.close()
