a147925d386b75dd89583a0193c966fc2c5cace4
YSassan <c1117817@planet.kanazawa-it.ac.jp>
2023-11-30 16:57:12 +0900

    feedparserのインポート URLの取得

--- getRss.py
+++ getRss.py
@@ -1,3 +1,7 @@
 # https://connpass.com/explore/ja.atom
 # 上のリンクのやつをうまいこと取得できるようにしてほしい
 # わかりづらかったら、コメントアウトめちゃめちゃ使っていいよ
+import feedparser
+
+url = "https://connpass.com/explore/ja.atom"
+f = feedparser.parser(url)
