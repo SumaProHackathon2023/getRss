90ca076384629e6160e99366b1db3c4fe7ca6dff
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-11-30 22:35:33 +0900

    add SendFirebase

---
+++ sendFiregase.py
@@ -0,0 +1,11 @@
+class SendFirebase():
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        print("test")
+
+
+if __name__ == "__main__":
+    sendFirebase = SendFirebase()
+    sendFirebase.main()
