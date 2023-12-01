5825c83b230bebc944cbce40f4f4d5ee148c5c0d
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-12-01 14:42:00 +0900

    add 任意のオブジェクトをfirestoreに追加

--- sendFiregase.py
+++ sendFiregase.py
@@ -1,4 +1,5 @@
 import firebase_admin
+from firebase_admin import credentials
 from firebase_admin import firestore
 
 # firebaseに取得した情報を送信するクラス
@@ -8,7 +9,8 @@ class SendFirebase():
 
     # 実際に送信するためのクラス定義
     def main(self):
-        app = firebase_admin.initialize_app()
+        cred = credentials.Certificate("./annoyingadvertisements-63b44-firebase-adminsdk-qf3k6-b1cd2eba56.json") # ダウンロードした秘密鍵
+        firebase_admin.initialize_app(cred)
         db = firestore.client()
         data = {"companyName": "sendFirebaseTest", "date": "sendFirebaseTest", "title": "sendFirebaseTest", "webLink": "sendFirebaseTest" }
         db.collection("event").add(data)
