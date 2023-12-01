9953768602607da38a53670f83f006b1c6134257
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-12-02 01:18:32 +0900

    add uploadFilter

---
+++ uploadFilter.py
@@ -0,0 +1,11 @@
+class UploadFilter():
+    def __init__(self) -> None:
+        pass
+
+    def main() -> None:
+        pass
+
+if __name__ == '__main__':
+    uploadFilter = UploadFilter()
+    uploadFilter.main()
+
