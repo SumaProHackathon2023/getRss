caf746b3263090c50c5cdf4c8aac900c47d86aea
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-12-02 11:06:03 +0900

    取得した情報をファイル書き込み部分を削除

--- getRss.py
+++ getRss.py
@@ -9,7 +9,3 @@ f = feedparser.parse(url)
 #print(len(f))
 #pprint.pprint(f)
 file = open('myfile.txt', 'w', encoding = 'UTF-8')
-for article in f.entries:
-    file.write('title:' + article.title + ' ' + 'link:' + article.link)
-    file.write('\n')
-file.close()
