40800a332d116c746968a81bdc2bc6a35206e83f
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-12-02 15:32:13 +0900

    役割の分散getRss -> uploadFilter

--- getRss.py
+++ getRss.py
@@ -6,8 +6,22 @@ import pprint
 from uploadFilter import UploadFilter
 
 url = "https://connpass.com/explore/ja.atom"
-f = feedparser.parse(url)
+rssData = feedparser.parse(url).entries
+uploadFilter = UploadFilter(rssData)
+# pprint.pprint(rssData)
 
-uploadFilter = UploadFilter(f)
+rssData = feedparser.parse(url).entries
+uploadFilter.updateRssData(rssData)
 while True:
-    uploadFilter.main()
+    rssData = feedparser.parse(url).entries
+    uploadFilter.updateRssData(rssData)
+
+# print(rssDta)
+
+#print(len(f))
+
+# file = open('myfile.txt', 'w', encoding = 'UTF-8')
+# for article in f.entries:
+#     file.write('title:' + article.title + ' ' + 'link:' + article.link)
+#     file.write('\n')
+# file.close()
--- uploadFilter.py
+++ uploadFilter.py
@@ -1,15 +1,33 @@
 import feedparser
 
 class UploadFilter():
-    def __init__(self, f: feedparser.util.FeedParserDict) -> None:
-        self.f = f
 
-    def main(self) -> None:
-        print(self.f.entries[0].title)
+    # コンストラクタ
+    def __init__(self, rssData: feedparser.util.FeedParserDict) -> None:
+        self.rssData = rssData #呼び出し元から受け取る変数を定義
+        self.new_Information = [] # 結果を格納するための変数
+        self.rssCash = rssData
+
+    # この関数をgetRss.pyでループして起動してく
+    def filtering(self, rssData) -> None:
+        for article in self.rssData.entries:
+            self.new_Information.append(article)
+
+    def updateRssData(self, rssData):
+        if self.rssCash[0] != rssData[0]:
+            print(self.rssCash[0])
+            print(rssData[0])
+            print("diff")
+            # 新しいイベントは何があるかを比較する
+            # 出した差分をsendFirebaseで送信処理を実行
+            # 差分用のself.rssCashに、新しい差分の一番新しいイベントを入れる
+            # self.rssCash = rssData.entries[0]
+        else:
+            print("あってる")
 
 if __name__ == '__main__':
     # 確認のためのデータ取得
     url = "https://connpass.com/explore/ja.atom"
     f = feedparser.parse(url)
-    uploadFilter = UploadFilter(f)
-    uploadFilter.main()
+    uploadFilter = UploadFilter(f.entries[0].title)
+    uploadFilter.filtering(f.entries[1].title)
