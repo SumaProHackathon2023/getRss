fe673c3d769d87cc4650e78bac9b1756f0ca3dda
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-12-02 19:08:14 +0900

    コメントアウト追加

--- getRss.py
+++ getRss.py
@@ -3,15 +3,20 @@ import pprint
 import time
 from uploadFilter import UploadFilter
 
+# rssDataを取得して、entriesのvalueを抽出
 url = "https://connpass.com/explore/ja.atom"
 rssData = feedparser.parse(url).entries
+
+# firebaseにあげるためのクラスのインスタンス生成
 uploadFilter = UploadFilter(rssData)
-rssData = feedparser.parse(url).entries
-uploadFilter.updateRssData(rssData)
+
+# rssのイベント追加をトリガーに、新しいイベントをfirebaseに送信する
 while True:
+    # rssの取得して、データ送信のための関数を呼び出す。
     rssData = feedparser.parse(url).entries
     uploadFilter.updateRssData(rssData)
     time.sleep(1)
+
     # 新しいイベントが追加されたかのテスト用 #
     s = [rssData[len(rssData) - _-1] for _ in range(len(rssData))] #entriesを反転させる
     uploadFilter.updateRssData(s)   #確認
