82db868053b28fb8932fdaa3508f31ec427e4c9f
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-12-02 19:17:03 +0900

    initとsendRssDataのリファクタリング

--- sendRssData.py
+++ sendRssData.py
@@ -8,22 +8,20 @@ class SendRssData():
     # コンストラクタ
     def __init__(self, rssData: feedparser.util.FeedParserDict) -> None:
         self.rssData = rssData #呼び出し元から受け取る変数を定義
-        self.new_Information = [] # 結果を格納するための変数
         self.rssCash = rssData[0]
 
     def sendRssData(self, rssData):
+        
+        # そもそも更新があるかどうかを判定
         if self.rssCash != rssData[0]:
-            #print(self.rssCash)
-            #print(rssData[0])
-            #print("diff")
             # 新しいイベントは何があるかを比較する
             rssDiff = self.differebcingRss(rssData)
+
             # 出した差分をsendFirebaseで送信処理を実行
-            self.sendFirebase(rssDiff)
+            self.setFirebase(rssDiff)
+
             # 差分用のself.rssCashに、新しい差分の一番新しいイベントを入れる
             self.rssCash = rssData[0]
-        else:
-            print("あってる")
 
     def differebcingRss(self, rssData) -> feedparser.util.FeedParserDict:
         """
@@ -43,7 +41,7 @@ class SendRssData():
         print(len(rssData[:count]))
         return rssData[:count]
 
-    def sendFirebase(self, rssDiff) -> None:
+    def setFirebase(self, rssDiff) -> None:
         cred = credentials.Certificate("./annoyingadvertisements-63b44-firebase-adminsdk-qf3k6-b1cd2eba56.json") # ダウンロードした秘密鍵
         firebase_admin.initialize_app(cred)
         db = firestore.client()
