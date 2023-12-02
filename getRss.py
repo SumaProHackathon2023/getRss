40e0096bc53d0f450d7678dd7318f610750eb141
Teraoka-Yuta <c1103854@planet.kanazawa-it.ac.jp>
2023-12-02 10:22:10 +0900

    開催日時と更新日時を追加

--- getRss.py
+++ getRss.py
@@ -4,12 +4,18 @@
 import feedparser
 import pprint
 
-url = "https://connpass.com/explore/ja.atom"
-f = feedparser.parse(url)
-#print(len(f))
-#pprint.pprint(f)
-file = open('myfile.txt', 'w', encoding = 'UTF-8')
-for article in f.entries:
-    file.write('title:' + article.title + ' ' + 'link:' + article.link)
-    file.write('\n')
-file.close()
+def getRss():
+    url = "https://connpass.com/explore/ja.atom"
+    f = feedparser.parse(url)
+    #print(len(f))
+    #pprint.pprint(f)
+    file = open('myfile.txt', 'w', encoding = 'UTF-8')
+    for article in f.entries:
+        file.write('title:' + article.title + ' ' + 'link:' + article.link + " 開催日時:"+article.summary[5:30] + " 更新日時:"+article.updated)
+    
+        print()
+        file.write('\n')
+    file.close()
+    return f.entries
+if __name__ == "__main__":
+     getRss()
