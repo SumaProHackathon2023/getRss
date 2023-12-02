84fc4a37789035619fc427477edd93f1ba929b82
Teraoka-Yuta <c1103854@planet.kanazawa-it.ac.jp>
2023-12-02 15:27:02 +0900

    更新されたかの判定と値の取り出し

--- getRss.py
+++ getRss.py
@@ -3,19 +3,57 @@
 # わかりづらかったら、コメントアウトめちゃめちゃ使っていいよ
 import feedparser
 import pprint
+import io, sys
+import time
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
-def getRss():
+
+
+def getRss(last_updated):
     url = "https://connpass.com/explore/ja.atom"
     f = feedparser.parse(url)
-    #print(len(f))
-    #pprint.pprint(f)
-    file = open('myfile.txt', 'w', encoding = 'UTF-8')
-    for article in f.entries:
-        file.write('title:' + article.title + ' ' + 'link:' + article.link + " 開催日時:"+article.summary[5:30] + " 更新日時:"+article.updated)
     
-        print()
-        file.write('\n')
-    file.close()
-    return f.entries
+    rssData = {}
+    # 更新がされているか判定
+    if last_updated != f.entries[0].updated:
+        for article in f.entries:
+            # 更新前の箇所かの判定
+            if article.updated != last_updated:
+                #タイトルをキーとする辞書を作成valueにはlinkと開催日、更新日を空白区切りで
+                rssData[article.title] = article.link +" "+ article.summary[5:30]+" "+article.updated
+            else:
+                break
+        last_updated = f.entries[0].updated
+        print(last_updated)
+        return rssData, True, last_updated
+    return "", False, last_updated
+
+
 if __name__ == "__main__":
-     getRss()
+    last_updated = ""
+
+    for i in range(10): #とりあえず10回
+        rssData, update, last_updated = getRss(last_updated)
+        print(update)
+        #if update:
+        #   print(rssData)
+        time.sleep(1)
+    """
+    rssData, last_updated = getRss(last_updated)
+    print(rssData)
+    time.sleep(1)
+    rssData, last_updated = getRss(last_updated)
+    print(rssData)
+    time.sleep(1)
+    """
+
+
+# print(rssData)
+#print(leen(f))
+#pprint.pprint(f)
+# file = open('myfile.txt', 'w', encoding = 'UTF-8')
+# for article in f.entries:
+#     file.write('title:' + article.title + ' ' + 'link:' + article.link)
+#     file.write('\n')
+# file.close()
