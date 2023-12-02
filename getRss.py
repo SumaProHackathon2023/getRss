0ded51f680eba942dc11e5b7fba6fd7655a5f0d2
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-12-02 19:02:48 +0900

    不要なコメント削除

--- getRss.py
+++ getRss.py
@@ -1,6 +1,3 @@
-# https://connpass.com/explore/ja.atom
-# 上のリンクのやつをうまいこと取得できるようにしてほしい
-# わかりづらかったら、コメントアウトめちゃめちゃ使っていいよ
 import feedparser
 import pprint
 import time
@@ -9,27 +6,15 @@ from uploadFilter import UploadFilter
 url = "https://connpass.com/explore/ja.atom"
 rssData = feedparser.parse(url).entries
 uploadFilter = UploadFilter(rssData)
-# pprint.pprint(rssData)
-
 rssData = feedparser.parse(url).entries
 uploadFilter.updateRssData(rssData)
 while True:
     rssData = feedparser.parse(url).entries
     uploadFilter.updateRssData(rssData)
     time.sleep(1)
-
     # 新しいイベントが追加されたかのテスト用 #
     s = [rssData[len(rssData) - _-1] for _ in range(len(rssData))] #entriesを反転させる
     uploadFilter.updateRssData(s)   #確認
-    ########################################
-
     time.sleep(60)
 
-# print(rssDta)
 
-#print(len(f))
-# file = open('myfile.txt', 'w', encoding = 'UTF-8')
-# for article in f.entries:
-#     file.write('title:' + article.title + ' ' + 'link:' + article.link)
-#     file.write('\n')
-# file.close()
